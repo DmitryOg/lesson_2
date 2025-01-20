1ac64d04082f53fb983c3fe3bde472c67f6af9dc
DmitryOg <euroset672602@gmail.com>
2025-01-20 18:46:19 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import transliterate
 from transliterate import translit
 from num2words import num2words
 
@@ -11,14 +10,8 @@ More than 3 years ago I moved to Novo-Novsk, but worked on new Magnetic
 Storage for last 40. When I was 8...""", 'ru'),'\n')
 
 
-num_1 = (num2words(78, lang='ru'))
-num_2 = (num2words(15, lang='ru'))
-num_3 = (num2words(3, lang='ru'))
-num_4 = (num2words(40, lang='ru'))
-num_5 = (num2words(8, lang='ru'))
-
-print("78 -", num_1)
-print("15 -", num_2)
-print("3 -", num_3)
-print("40 -", num_4)
-print("8 -", num_5)
+print("78 -",  translit((num2words(78)),'ru'))
+print("15 -", translit((num2words(15)),'ru'))
+print("3 -",  translit((num2words(3)),'ru'))
+print("40 -",  translit((num2words(40)),'ru'))
+print("8 -", translit((num2words(8)),'ru'))
